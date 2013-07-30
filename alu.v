23d9e4caa1d294caa2aee108f66ca70a27a5cdda
Michael Pratt <michael@pratt.im>
2013-07-29 23:03:37 -0700

    Remove clock from ALU, add PASSA to ALU tests

--- alu.v
+++ alu.v
@@ -1,5 +1,4 @@
-module ALU(clk, A, B, ALUK, OUT);
-    input wire clk;
+module ALU(A, B, ALUK, OUT);
     input wire [15:0]A;
     input wire [15:0]B;
     input wire [1:0]ALUK;
@@ -8,14 +7,14 @@ module ALU(clk, A, B, ALUK, OUT);
     parameter ADD = 2'd0;
     parameter AND = 2'd1;
     parameter XOR = 2'd2;
-    parameter PASS = 2'd3;
+    parameter PASSA = 2'd3;
 
-    always @ (posedge clk) begin
+    always @ (A or B or ALUK) begin
         case(ALUK)
             ADD: OUT <= A + B;
             AND: OUT <= A & B;
             XOR: OUT <= A ^ B;
-            PASS: OUT <= A;     // Which input am I supposed to pass?
+            PASSA: OUT <= A;
         endcase
     end
 endmodule
--- test.v
+++ test.v
@@ -1,4 +1,4 @@
-`define ALU_TEST(__a, __b, __add, __and, __xor) \
+`define ALU_TEST(__a, __b, __add, __and, __xor, __passa) \
     A = __a;    \
     B = __b;    \
     #2          \
@@ -7,7 +7,9 @@
     if (OUT_AND != __and)   \
         $display("0x%h & 0x%h = 0x%h, expected 0x%h", __a, __b, OUT_AND, __and);  \
     if (OUT_XOR != __xor)   \
-        $display("0x%h ^ 0x%h = 0x%h, expected 0x%h", __a, __b, OUT_XOR, __xor);
+        $display("0x%h ^ 0x%h = 0x%h, expected 0x%h", __a, __b, OUT_XOR, __xor);  \
+    if (OUT_PASSA != __passa)   \
+        $display("0x%h = 0x%h, expected 0x%h", __a, __b, OUT_PASSA, __passa);
 
 module alu_test;
     reg [15:0]A;
@@ -15,19 +17,20 @@ module alu_test;
     wire [15:0]OUT_ADD;
     wire [15:0]OUT_AND;
     wire [15:0]OUT_XOR;
+    wire [15:0]OUT_PASSA;
 
-    LC3 cpu();
-    ALU test_add(cpu.clk, A, B, test_add.ADD, OUT_ADD);
-    ALU test_and(cpu.clk, A, B, test_and.AND, OUT_AND);
-    ALU test_xor(cpu.clk, A, B, test_xor.XOR, OUT_XOR);
+    ALU test_add(A, B, test_add.ADD, OUT_ADD);
+    ALU test_and(A, B, test_and.AND, OUT_AND);
+    ALU test_xor(A, B, test_xor.XOR, OUT_XOR);
+    ALU test_passa(A, B, test_xor.PASSA, OUT_PASSA);
 
     initial begin
         $dumpfile("alu_test.vcd");
         $dumpvars;
 
-        `ALU_TEST(1, 1, 2, 1, 0)
-        `ALU_TEST(2, 1, 3, 0, 3)
-        `ALU_TEST(16'h1000, 16'h0001, 16'h1001, 0, 16'h1001)
+        `ALU_TEST(1, 1, 2, 1, 0, 1)
+        `ALU_TEST(2, 1, 3, 0, 3, 2)
+        `ALU_TEST(16'h1000, 16'h0001, 16'h1001, 0, 16'h1001, 16'h1000)
 
         $finish;
     end
